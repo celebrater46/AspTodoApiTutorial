55fb6c8d85674827a49d4eee75d231db10893b49
hideru <jota.16.stigma.world@gmail.com>
2021-08-23 17:38:27 +0900

    First Commit (After Adding TodoItemDTO, too late...)

---
+++ TodoApi210823/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi210823.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
